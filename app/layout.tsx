71741e10781170c0583d645795dee379d976ec6b
Royweru <royezi17@gmail.com>
2024-05-23 10:50:07 +0300

    modified:   app/globals.css 	modified:   app/layout.tsx 	modified:   app/page.tsx 	new file:   components/hero.tsx 	new file:   components/ui/Spotlight.tsx 	modified:   package-lock.json 	modified:   package.json 	modified:   tailwind.config.ts 	renamed:    lib/utils.ts -> utils/cn.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className=" w-full min-h-screen  p-0">
+
         {children}
-        </main>
+     
 
         </body>
     </html>
