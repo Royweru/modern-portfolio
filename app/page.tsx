5e0554757dfee283861631b9a3a9e9831395defd
Royweru <royezi17@gmail.com>
2024-06-09 13:08:05 +0300

    modified:   app/page.tsx 	modified:   components/grid.tsx 	modified:   components/ui/bento-grid.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Grid } from "@/components/grid";
 import { Hero } from "@/components/hero";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { BiHome } from "react-icons/bi";
@@ -19,14 +20,14 @@ export default function Home() {
       link: "/",
       icon: <BiHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
- 
   ];
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-    <div className="max-w-7xl w-full">
-       <FloatingNav navItems={navItems}/>
-      <Hero />
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
       </div>
-      </main>
+    </main>
   );
 }
