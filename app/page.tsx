dde022f39802f0441c6b0aa0fbffcfb007ddb87c
Royweru <royezi17@gmail.com>
2024-10-05 09:16:36 +0300

    modified:   app/page.tsx 	modified:   components/ui/floating-navbar.tsx 	modified:   data/index.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
 import { Approach } from "@/components/approach";
 import { Techtools } from "@/components/techtools";
-import { Experience } from "@/components/experience";
 import { Grid } from "@/components/grid";
 import { Hero } from "@/components/hero";
 import { RecentProjects } from "@/components/recent-projects";
 import Footer from "@/components/footer";
 import { Navbar } from "@/components/navigation/navbar";
 import { Planning } from "@/components/planning";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className=" max-w-6xl w-full overflow-hidden">
-        <Navbar />
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems}/>
         <Hero />
         <Grid />
         <RecentProjects />
